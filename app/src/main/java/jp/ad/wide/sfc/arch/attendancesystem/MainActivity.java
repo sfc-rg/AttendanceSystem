4d75371104ed0eacd362a61c92dd941e05d148e6
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-09-24 00:17:18 +0900

    add some textview and set screen orientation as portrait

--- app/src/main/java/jp/ad/wide/sfc/arch/attendancesystem/MainActivity.java
+++ app/src/main/java/jp/ad/wide/sfc/arch/attendancesystem/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
